74063fc5b824eb0e7b39511c01ef7fc2f2ad8fe7
Abdessattar <ghabi.1602@gmail.com>
2024-03-22 19:58:19 -0700

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# installs the package puppet-lint
+# installs puppet-lint package
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  name     => 'flask',
+  provider => 'pip3'
 }
